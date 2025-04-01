e24fb049c09eb6fe8f7b7e71313fdf0f8f57ad22
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2025-04-01 21:26:47 +0530

    change the platform windows to ubuntu

--- backend/src/Routers/setup_folder.ts
+++ backend/src/Routers/setup_folder.ts
@@ -4,7 +4,7 @@ import { locations } from "../Data/data";
 import Services from "../Services";
 import create_user_folder from "../Services/create_user_folder.js";
 export default setup_folder.post('/', (req, res) => {
-   locations.user_folder_loactions = req.body.name;
+   locations.user_folder_loactions = req.body.id;
    create_user_folder();
    Services.setup_site_folder();
    res.send({
