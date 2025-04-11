054b4540cc964c0a1a0c5415ae565b1391330abf
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2025-04-11 12:29:19 +0530

    fix the service page nevigation bug

--- frontend/src/Redux/Actions/HostStaticSite.js
+++ frontend/src/Redux/Actions/HostStaticSite.js
@@ -3,7 +3,7 @@ import { set_crspgif } from "./SetCrsrpgif.js";
 export const host_static_website = ({ files, websiteName, domainName }) => {
     return async (dispatch, getState) => {
         try {
-            const state = getState();
+            let state = getState();
             const formData = new FormData();
             files.forEach((file, index) => {
                 formData.append("files", file);
@@ -15,12 +15,11 @@ export const host_static_website = ({ files, websiteName, domainName }) => {
             formData.append("user_name", state.Data.UserInfo.user.name);
             dispatch({ type: 'NVGT_TO_SITE', payload: false })
             dispatch({ type: 'SHOW_LOADER', payload: true })
-            console.log("loading start =>", state.Data.ComponentData.show_file_loader);
+            //  console.log("loading start =>", state.Data.ComponentData.show_file_loader);
 
             fetch(import.meta.env.VITE_BACKEND_URL + "/host_site", {
                 method: 'POST',
                 body: formData,
-
             }).then((response) => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -28,16 +27,20 @@ export const host_static_website = ({ files, websiteName, domainName }) => {
                 return response.json();
             }
             ).then((response) => {
-                console.log("Response data:", response);
-                set_crspgif(response?.data?.site);
-                dispatch({ type: 'SET_CRSRPGIF', payload: state });
+             //   console.log("Response data:------>", response.site);
+                dispatch(set_crspgif(response.site));
+         
+                const updatedState = getState();
+             //   console.log("checking in thunk SET_CRSRPGIF: ", updatedState.Data.UserInfo.other_info.crsrpgif);
                 dispatch({ type: 'SHOW_LOADER', payload: false });
                 dispatch({ type: 'NVGT_TO_SITE', payload: true });
+             //   console.log("checking in thunk nvgt_to_site : ", updatedState.Data.ComponentData.nvgt_to_site);
+
                 return response;
             })
-        
-        
-           
+
+
+
         } catch (error) {
             console.error("Error uploading files:", error);
         }
